1289d4cfb8745fdbb6dc095a754e0cefd11d57df
JoshuaSanchez <misael_san12@hotmail.com>
2025-07-29 20:16:56 -0600

    nueva actualizacion de mi proyecto

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Tripleten web_project_around
+
+Around the U.S. Es un ejercicio de desarrollo web donde se practica la implementacion de programacion Java Script junto con otras técnologias aprendias, como HTML y CSS, mediante la construccion de una ventana emergente dentro de la misma pagina web donde se muestran formularios que actualizan la informacion del perfil principal.
+
+## Características
+
+El proyecto muestra una pagina web con un perfil de usuario modificable, a traves de formularios web. Tambien incluye una galeria de imagenes de lugares turisticos en Estados Unidos.
+
+## Estructura del proyecto
+
+├── images/
+│ ├── logo.svg
+│ ├── Like-btn.svg
+│ ├── lago-braises.png
+│ ├── lago-louise.png
+│ ├── montañas-calvas.png
+│ ├── valle-yosemite.png
+│ ├── vanois-national-park.png
+│ └── profile.png
+├── blocks/
+│ ├── page.css
+│ ├── popup.css
+│ ├── header.css
+│ ├── main.css
+│ └── footer.css
+├── pages/
+│ └── index.css
+├── Vendor/
+│ ├── fonts/
+│ │ ├── Inter-Black.woff2
+│ │ └── Inter-Regular.woff2│
+│ ├── fonts.css
+│ └── normalize.css
+├── index.html
+├── .edittorconfig
+├── .gitignore
+└── README.md
+
+## Técnologias utilizadas
+
+- HTML5
+- CSS3
+- Google Fonts
+- JavaScript
+
+## Autor
+
+Joshua Sánchez
+
+## Enlace GitHub
+
+https://joshuasanchez21.github.io/web_project_homeland/
---
+++ script/script.js
@@ -0,0 +1,46 @@
+let editButton = document.querySelector(".main__button_edit");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__button_close");
+let form = document.querySelector(".popup__container");
+let inName = document.querySelector(".main__description_name");
+let inAbout = document.querySelector(".main__description_role");
+let inpName = document.querySelector(".popup__input_name");
+let inpAbout = document.querySelector(".popup__input_about");
+let saveButton = document.querySelector(".popup__button_save");
+
+function openEditEmpty() {
+  inpName.value = "";
+  inpAbout.value = "";
+  saveButton.disabled = true;
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function openEdit() {
+  inpName.value = "";
+  inpAbout.value = "";
+  validateInputs();
+
+  popup.classList.toggle("popup_opened");
+}
+function validateInputs() {
+  const isNameValid = inpName.value.trim() !== "";
+  const isAboutValid = inpAbout.value.trim() !== "";
+  saveButton.disabled = !(isNameValid && isAboutValid);
+}
+inpName.addEventListener("input", validateInputs);
+inpAbout.addEventListener("input", validateInputs);
+editButton.addEventListener("click", openEdit);
+closeButton.addEventListener("click", openEdit);
+
+function saveChange(e) {
+  e.preventDefault();
+  inName.textContent = inpName.value;
+  inAbout.textContent = inpAbout.value;
+  openEdit();
+}
+
+form.addEventListener("submit", saveChange);
